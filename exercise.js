56aa4eec21c03f5fc6332bee01324dbe56372100
duvan-monroy <duvanmonroy1127@gmail.com>
2023-09-15 21:12:21 -0500

    ejercico javascript

---
+++ exercise.js
@@ -0,0 +1,8 @@
+var respuesta =prompt("ingrese un numero");
+if(respuesta >= 1 ){
+    alert('es positivo');
+} else if(respuesta < 0){
+    alert('es negativo');
+}else {
+    alert('el numero es 0')
+}
